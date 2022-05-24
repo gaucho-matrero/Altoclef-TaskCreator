ed6844bfc9cf18a396978dd8ddf2986ca4bcc866
James Green <james@p.ckplace.org>
2022-05-24 17:23:21 -0600

    Created menu's for adding new tasks and added partial functionality
    We still need to add a few more buttons, but the basic logic is all here.
    
    Signed-off-by: James Green <james@p.ckplace.org>

--- src/main/java/james/altoclef/taskcreator/graphics/NewTaskFrame.java
+++ src/main/java/james/altoclef/taskcreator/graphics/NewTaskFrame.java
@@ -1,11 +1,13 @@
 package james.altoclef.taskcreator.graphics;
 
+import james.altoclef.taskcreator.customSubTask;
 import james.altoclef.taskcreator.interfaces.Key;
 import james.altoclef.taskcreator.writeableTask;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -25,126 +27,108 @@ public class NewTaskFrame extends JDialog {
     private JPanel main_panel;
     private JTextField tf_name;
     private JTextField tf_desc;
-    private writeableTask customTask;
-    private List<writeableTask> subTasks;
+
+    private final writeableTask taskToWrite;
+    private List<customSubTask> subTaskList;
+
     public NewTaskFrame() {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setModal(true);
         setContentPane(main_panel);
-        subTasks = new ArrayList<>();
-        customTask = null;
         initComponents();
         setSize(500, 300);
         setLocationRelativeTo(null);
-        setVisible(true);
         getRootPane().setDefaultButton(btn_save);
-
+        taskToWrite = null;
+        subTaskList = new ArrayList<customSubTask>();
+        setVisible(true); //must always be last
     }
-    public NewTaskFrame(writeableTask customTask) { //for edit
-        setContentPane(main_panel);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.customTask = customTask;
-        initComponents();
-    }
-
-    private void initComponents() {
-        // Command combo box
-        commandBox.addItem("goto");
-        commandBox.addItem("get");
-        commandBox.addItem("punk");
-        commandBox.addItem("deposit");
-        commandBox.addItem("gamer");
-        commandBox.setSelectedItem("get");
-        //parameter dialogue
-        if(Objects.requireNonNull(commandBox.getSelectedItem()).toString().equals("")){
-            btn_add_sub_task.setEnabled(false);
-        }
-        {
-            btn_add_sub_task.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    subtasks_parameterizer stp;
-                    switch ((Objects.requireNonNull(commandBox.getSelectedItem()).toString())) {
-                        case "goto" -> stp = new subtasks_parameterizer("goto");
-                        case "get" -> stp = new subtasks_parameterizer("get");
-                        case "punk" -> stp = new subtasks_parameterizer("punk");
-                        default -> doNothing();
-                    }
-
-                }
-
-                private void doNothing() {
-                    //does nothing. Should never happen
-                }
-            });
-            btn_save.addActionListener(new ActionListener() {
-                /**
-                 * Invoked when an action occurs.
-                 *
-                 * @param e the event to be processed
+    
+    private void initComponents(){
+        btn_add_sub_task.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /*
+                TODO
+                    1. Launch sub task parameterizer dialogue [DONE]
+                    2. Store the results to subTaskList [DONE]
+                    3. refresh table [DONE]
+                    [future]
+                        Allow for selection of items in the table
                  */
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    if(tf_name.getText().isBlank()|| tf_desc.getText().isBlank()){
-                        AltoJsonWarning warning = new AltoJsonWarning("Name and description cannot be blank");
-                    }else{
-                        JSONArray toStore = new JSONArray();
-                        customTask = new writeableTask();
-                        customTask.add(new Key() {
-                            @Override
-                            public String getKey() {
-                                return "name";
-                            }
-
-                            @Override
-                            public Object getValue() {
-                                return tf_name.getText();
-                            }
-                        });
-                        customTask.add(new Key() {
-                            @Override
-                            public String getKey() {
-                                return "description";
-                            }
-
-                            @Override
-                            public Object getValue() {
-                                return tf_desc.getText();
-                            }
-                        });
-                        for (writeableTask t : subTasks) {
-                            toStore.put(t.writeObject());
-                        }
-                        customTask = new writeableTask();
-                        customTask.add(new Key() {
-                            @Override
-                            public String getKey() {
-                                return "tasks";
-                            }
-
-                            @Override
-                            public Object getValue() {
-                                return toStore;
-                            }
-                        });
-                        dispose();
-                    }
-                    //write(); may or may not be needed
-                }
-            });
-        }//buttons
-
+                subtasks_parameterizer task_parameters_dialogue = new subtasks_parameterizer(commandBox.getActionCommand());
+                //compile tasks array to a json object
+                subTaskList.add(task_parameters_dialogue.getItems());
+                refreshTable();
+            }
+        });
+        setVisible(true);//must always be last
     }
 
+    private void refreshTable() {
+        DefaultTableModel model = new DefaultTableModel(new String[]{"command type","command_param_preview"},0);
+        subtask_table.setModel(model);
+        try{
+            for(customSubTask t: subTaskList){
+                model.addRow(new Object[]{t.getType(),t.getParameters()}); //could break if someone had a HUGE task
+            }
+            subtask_table.setModel(model);
+        }catch (Exception ignored){
+
+        }
+    }
+
+    /**
+     * compiles the components of a writeable task into a JSON
+     * @return the compiled components
+     */
     public JSONObject write() {
-        //TODO temp, use writeableTask.write
-        return customTask.writeObject();
-    }
+        //name
+        taskToWrite.add(new Key() {
+            @Override
+            public String getKey() {
+                return "name";
+            }
 
-    public static void main(String[] args) {
-        NewTaskFrame nTF = new NewTaskFrame();
-        nTF.pack();
-        nTF.setVisible(true);
-        System.exit(0);
+            @Override
+            public Object getValue() {
+                return tf_name;
+            }
+        });
+        //description
+        taskToWrite.add(new Key() {
+            @Override
+            public String getKey() {
+                return "description";
+            }
+
+            @Override
+            public Object getValue() {
+                return tf_desc;
+            }
+        });
+        //tasks
+        JSONArray subTasks = new JSONArray();
+        for(customSubTask t: subTaskList){
+            subTasks.put(t.build());
+        }
+        taskToWrite.add(new Key() {
+            @Override
+            public String getKey() {
+                return "tasks";
+            }
+
+            @Override
+            public Object getValue() {
+                return subTasks;
+            }
+        });
+        return taskToWrite.writeObject();
+
     }
 }
--- src/main/java/james/altoclef/taskcreator/graphics/subtasks_parameterizer.java
+++ src/main/java/james/altoclef/taskcreator/graphics/subtasks_parameterizer.java
@@ -7,6 +7,9 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class subtasks_parameterizer extends JDialog {
     private JPanel main_panel;
@@ -16,86 +19,25 @@ public class subtasks_parameterizer extends JDialog {
     private JButton btn_plus;
     private JButton btn_done;
     private JLabel l_type;
-    private final String command;
-    private JSONArray subTasks;
 
+    private final List<Object> params;
+    private final String command;
     public subtasks_parameterizer(String command) {
         setContentPane(main_panel);
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         setSize(400, 250);
         setModal(true);
         this.command = command;
-        initComponents();
-
+        params = new ArrayList<Object>();
+        setVisible(true); // must always be last
     }
 
     private void initComponents() {
-        l_type.setText(command);
-        switch (command) {
-            case "get" -> {
-                item_selector.setVisible(true);
-                item_selector.addItem("diamond");
-            }
-            //other items here
-            case "goto" -> {
-                item_selector.setVisible(true);
-                item_selector.addItem("overworld");
-                item_selector.addItem("nether");
-            }
-            default -> item_selector.setVisible(false);
-
-
-        }
-        {
-            btn_done.addActionListener(new ActionListener() {
-                /**
-                 * Invoked when an action occurs.
-                 *
-                 * @param e the event to be processed
-                 */
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    dispose();
-                }
-            });
-
-            btn_plus.addActionListener(new ActionListener() {
-                /**
-                 * Invoked when an action occurs.
-                 *
-                 * @param e the event to be processed
-                 */
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    try {
-                        customSubTask cST = null; //TODO add the other cases
-                        switch (command) {
-                            case "get":
-                                cST = new customSubTask("get", new Object[]{item_selector.getItemAt(0), Integer.parseInt(broad_parameter.getText())});
-                        }
-                        subTasks.put(cST);
-                    } catch (Exception ex) {
-                        AltoJsonWarning aw = new AltoJsonWarning("Unable to add command");
-                    }
-                }
-            });
-
-        }//action listeners
-        setVisible(true);
-
+        //TODO create action listeners. See New Task Frame
     }
 
-    private void refreshTable() {
-        DefaultTableModel model = new DefaultTableModel(new String[]{"Command", "param"}, 0) {
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                return false;
-            }
-        };
-        table1.setModel(model);
 
-        for (int i = 0; i < subTasks.length(); i++) {
-            //TODO add the rows of the subtask list.
-        }
+    public customSubTask getItems() {
+       return new customSubTask(l_type.getText(),params.toArray());
     }
 }
