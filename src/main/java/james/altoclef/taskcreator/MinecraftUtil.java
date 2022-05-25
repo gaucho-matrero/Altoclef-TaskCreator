bb7a038ea55c9fc88cba865a55a8ba31f63f83d5
James Green <james@p.ckplace.org>
2022-05-25 00:36:45 -0600

    Save point
    Signed-off-by: James Green <james@p.ckplace.org>

---
+++ src/main/java/james/altoclef/taskcreator/MinecraftUtil.java
@@ -0,0 +1,11 @@
+package james.altoclef.taskcreator;
+
+public class MinecraftUtil {
+    //list of items
+    private static String[] items = new String[]{
+
+    };
+    public static String[] getItems() {
+        return items;
+    }
+}
