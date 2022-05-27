d84136cee88c69cb09e9ce38e3b603c9d8ecacbd
James Green <james@p.ckplace.org>
2022-05-26 19:20:17 -0600

    Creating save point. Almost finished gui. Only need to finish the write method for NewTaskFrame and the edit button. Fromn then, we can actually start writing JSON files.
    Signed-off-by: James Green <james@p.ckplace.org>

--- src/main/java/james/altoclef/taskcreator/graphics/NewTaskFrame.java
+++ src/main/java/james/altoclef/taskcreator/graphics/NewTaskFrame.java
@@ -15,7 +15,6 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class NewTaskFrame extends JDialog {
     private JTable subtask_table;
@@ -23,16 +22,17 @@ public class NewTaskFrame extends JDialog {
     private JLabel l_command;
     private JButton btn_add_sub_task;
     private JLabel l_param;
-    private JButton btn_add;
     private JButton btn_Remove;
     private JButton btn_save;
     private JButton btn_Cancel;
     private JPanel main_panel;
     private JTextField tf_name;
     private JTextField tf_desc;
+    private JButton btn_clearAll;
+    private JButton btn_copy;
 
-    private final writeableTask taskToWrite;
-    private List<customSubTask> subTaskList;
+    private final writeableTask taskToWrite = new writeableTask();
+    private final List<customSubTask> subTaskList;
 
     public NewTaskFrame() {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -42,7 +42,6 @@ public class NewTaskFrame extends JDialog {
         setSize(950, 520);
         setLocationRelativeTo(null);
         getRootPane().setDefaultButton(btn_save);
-        taskToWrite = null;
         subTaskList = new ArrayList<customSubTask>();
         initComponents();
         setVisible(true);//must always be last
@@ -68,23 +67,46 @@ public class NewTaskFrame extends JDialog {
                 }else {
                     if (subtask_table.getSelectedRow() != -1) {
                         btn_add_sub_task.setText("Edit");
+                        btn_copy.setEnabled(true);
+                        btn_Remove.setEnabled(true);
                     }
                 }
             }
 
 
         });
-        btn_add_sub_task.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               openSubtaskMenu();
+        btn_add_sub_task.addActionListener(e->{
+            openSubtaskMenu();
+        });
+        btn_Remove.addActionListener(e->{
+            if(subtask_table.getSelectedRow()!=-1){
+                subTaskList.remove(subtask_table.getSelectedRow());
+                refresh();
             }
         });
+        btn_copy.addActionListener(e -> {
+            if(subtask_table.getSelectedRow()!=-1){
+                subTaskList.add(subTaskList.get(subtask_table.getSelectedRow()));
+                refresh();
+            }
+        });
+        btn_clearAll.addActionListener(e -> {
+
+            AltoJsonWarning confirmation = new AltoJsonWarning("Warning: Clear all", "Are you sure you want to clear all items?");
+            if(confirmation.OKPressed()){
+                subTaskList.clear();
+            }
+            refresh();
+        });
+        btn_save.addActionListener(e->{
+            dispose();
+        });
+        btn_Cancel.addActionListener(e->{
+            subTaskList.clear();
+            dispose();
+        });
+
+        refresh();
     }
 
     private void openSubtaskMenu() {
@@ -106,7 +128,7 @@ public class NewTaskFrame extends JDialog {
             isEdit=true;
             editIndex = subtask_table.getSelectedRow();
             subTaskList.remove(subtask_table.getSelectedRow());
-            refreshTable();
+            refresh();
         }else {
             task_parameters_dialogue =
                     new subtasks_parameterizer(commandBox.getSelectedItem().toString()); //Ignore warning, will never be null
@@ -119,10 +141,11 @@ public class NewTaskFrame extends JDialog {
             if (!task_parameters_dialogue.shouldDiscard())
                 subTaskList.add(task_parameters_dialogue.getItems());
         } //TODO Allow multiple thins added
-        refreshTable();
+        refresh();
     }
 
-    private void refreshTable() {
+    private void refresh() {
+        //Refresh Table
         DefaultTableModel model = new DefaultTableModel(new String[]{"command type","command_param_preview"},0){
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -141,7 +164,14 @@ public class NewTaskFrame extends JDialog {
         }catch (Exception ignored){
 
         }
+        //refresh buttons
         btn_add_sub_task.setText("+");
+        btn_Remove.setEnabled(false);
+        btn_copy.setEnabled(false);
+        btn_clearAll.setEnabled(subTaskList.size() > 0);
+
+       //TODO disable save button in bad instances
+
     }
 
     /**
@@ -149,6 +179,18 @@ public class NewTaskFrame extends JDialog {
      * @return the compiled components
      */
     public JSONObject write() {
+        if(subTaskList.size()==0){
+            throw new NullPointerException("Saved task contains no items");
+        }
+        if(tf_name.getText().isBlank() || tf_name.getText().isEmpty() || tf_name.getText().contains(" ")){ // no w h i t e s p a c e
+            throw new NullPointerException("Saved task contains invalid name");
+        }
+        if(tf_desc.getText().isBlank() || tf_desc.getText().isEmpty()){
+            throw new NullPointerException("Saved task contains invalid description");
+        }
+
+//TODO Check for integers
+
         //name
         taskToWrite.add(new Key() {
             @Override
@@ -158,7 +200,7 @@ public class NewTaskFrame extends JDialog {
 
             @Override
             public Object getValue() {
-                return tf_name;
+                return tf_name.getText();
             }
         });
         //description
@@ -170,7 +212,7 @@ public class NewTaskFrame extends JDialog {
 
             @Override
             public Object getValue() {
-                return tf_desc;
+                return tf_desc.getText();
             }
         });
         //tasks
